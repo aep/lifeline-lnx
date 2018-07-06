429a040b567c6997d920b7e636caabc70ca21722
Arvid E. Picciani <aep@exys.org>
2018-07-06 14:27:08 +0200

    m

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate tokio;
 extern crate nix;
 extern crate tokio_file_unix;
 extern crate libc;
+#[macro_use] extern crate log;
+extern crate env_logger;
+extern crate ws;
 
 use std::net::{TcpStream};
 use ssh2::Session;
@@ -11,24 +14,92 @@ use std::path::Path;
 use std::thread;
 use nix::poll;
 use std::os::unix::io::AsRawFd;
+use std::os::unix::io::FromRawFd;
+use std::fs::File;
+use std::env;
 
 
+fn tcppair() -> (TcpStream, TcpStream) {
+    use nix::sys::socket;
+    let (fd1, fd2) = socket::socketpair(
+        socket::AddressFamily::Unix,
+        socket::SockType::Stream,
+        None,
+        socket::SockFlag::SOCK_NONBLOCK,
+        ).unwrap();
+
+    unsafe {(
+        TcpStream::from_raw_fd(fd1),
+        TcpStream::from_raw_fd(fd2)
+    )}
+}
 
 
-fn mainloop() {
-    // Connect to the local SSH server
-    let tcp = TcpStream::connect("127.0.0.1:22").unwrap();
+struct WsHandler (TcpStream);
+
+impl ws::Handler for WsHandler {
+    fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
+        match msg {
+            ws::Message::Text(text)  => eprintln!("::> {}", text),
+            ws::Message::Binary(bin) => {
+                self.0.write(&bin).unwrap();
+            },
+        }
+        Ok(())
+    }
+}
+
+fn wsb(tcp: TcpStream) {
+
+    let mut getme = Some(tcp);
+    eprintln!("::> connecting to {}", env::args().nth(1).unwrap());
+    let wsurl = format!("wss://lifeline.superscale.io/lifelines/{}/ssh", env::args().nth(1).unwrap());
+
+
+    if let Err(error) = ws::connect(wsurl, |out|{
+        let tcp = std::mem::replace(&mut getme, None).unwrap();
+
+        let out2 = out.clone();
+        let fd = nix::unistd::dup(tcp.as_raw_fd()).unwrap();
+        let mut fr = unsafe{ std::net::TcpStream::from_raw_fd(fd)};
+        thread::spawn(move ||{
+            let mut buf = [0;1024];
+            loop {
+                let mut fds = [poll::PollFd::new(fd, poll::EventFlags::POLLIN)];
+                poll::poll(&mut fds, -1).unwrap();
+
+                let len = fr.read(&mut buf).unwrap();
+                if len < 1 {
+                    //TODO ws close
+                    return;
+                }
+                out.send(&buf[..len]).unwrap();
+            }
+        });
+
+        WsHandler(tcp)
+    }){
+        error!("Failed to create WebSocket due to: {:?}", error);
+    };
+    std::process::exit(0);
+}
+
+
+fn mainloop(tcp: TcpStream) {
     let mut sess = Session::new().unwrap();
     sess.handshake(&tcp).unwrap();
-    sess.userauth_pubkey_file("aep", None, Path::new("/home/aep/.ssh/id_rsa"), None);
+    sess.userauth_pubkey_file("root", None, Path::new("/home/aep/.ssh/id_rsa"), None);
 
     assert!(sess.authenticated());
-
 
     let mut channel = sess.channel_session().unwrap();
     channel.request_pty("vt220", None, None).unwrap();
     channel.shell().unwrap();
     sess.set_blocking(false);
+
+    let stdin = tokio_file_unix::raw_stdin().unwrap();
+    let stdin_fd   = stdin.as_raw_fd();
+    set_raw_mode(stdin_fd);
 
     let mut stdout = io::stdout();
     let stdin = tokio_file_unix::raw_stdin().unwrap();
@@ -37,9 +108,7 @@ fn mainloop() {
     let stdin_fd   = stdin.as_raw_fd();
     let channel_fd = tcp.as_raw_fd();
 
-
     loop {
-
         let mut fds = [
             poll::PollFd::new(stdin_fd, poll::EventFlags::POLLIN),
             poll::PollFd::new(channel_fd, poll::EventFlags::POLLIN),
@@ -55,7 +124,7 @@ fn mainloop() {
                     Ok(len) =>  len,
                     Err(ref e) if e.kind() == io::ErrorKind::WouldBlock  => break,
                     Err(e) => {
-                        println!("{}", e);
+                        error!("{}", e);
                         return;
                     }
                 };
@@ -74,7 +143,7 @@ fn mainloop() {
                         if io::Error::last_os_error().kind()  == io::ErrorKind::WouldBlock {
                             break;
                         } else {
-                            println!("{}", e);
+                            error!("{}", e);
                             return;
                         }
                     }
@@ -94,16 +163,25 @@ fn mainloop() {
 }
 
 fn main() {
+    env_logger::init();
+
+
+    let (t1, t2) = tcppair();
+
+
+    thread::spawn(||{
+        wsb(t1);
+    });
+
 
     let stdin = tokio_file_unix::raw_stdin().unwrap();
     let stdin_fd   = stdin.as_raw_fd();
 
     let mut termios = nix::sys::termios::tcgetattr(stdin_fd).unwrap();
     let saved_termios = termios.clone();
-    set_raw_mode(stdin_fd);
 
 
-    mainloop();
+    mainloop(t2);
 
     nix::sys::termios::tcsetattr(stdin_fd,
                                  nix::sys::termios::SetArg::TCSADRAIN,
